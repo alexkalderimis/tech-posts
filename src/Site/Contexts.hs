ceeab83791a6200d00acd5f49e836f1a19556c2b
Alex Kalderimis <akalderimis@gitlab.com>
2021-04-24 22:14:53 +0100

    Update social information

--- src/Site/Contexts.hs
+++ src/Site/Contexts.hs
@@ -15,7 +15,7 @@ import Data.Time.Clock
 import Data.Time.Calendar
 
 repo :: String
-repo = "https://github.com/alexkalderimis/tech-post"
+repo = "https://gitlab.com/alexkalderimis/tech-posts"
 
 baseContext :: Context String
 baseContext = defaultContext <> avatarContext
@@ -52,7 +52,7 @@ gitTag :: String -> Context String
 gitTag key = field key $ \_ -> unsafeCompiler $ do
     sha     <- readProcess "git" ["log", "-1", "HEAD", "--pretty=format:%H"] []
     message <- readProcess "git" ["log", "-1", "HEAD", "--pretty=format:%s"] []
-    return $ mconcat ["<a href=\"", repo, "/commit/", sha
+    return $ mconcat ["<a href=\"", repo, "/-/commit/", sha
                      , "\" title=\"", message, "\">"
                      , take 8 sha, "</a>"
                      ]
